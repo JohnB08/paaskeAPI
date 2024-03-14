4a4062cf4a4b293e09a25a9ad04ed7889b345af8
JohnB08 <johnb08.kodehode@gmail.com>
2024-03-14 12:43:28 +0000

    jtw verification

--- userfuncs/createUser.js
+++ userfuncs/createUser.js
@@ -1,4 +1,4 @@
-import { createUserInDB, getAllUsernames } from "../pgFuncs/dbfuncs.js";
+import { createUserInDB, } from "../pgFuncs/dbfuncs.js";
 import jwt from "jsonwebtoken";
 export const createNewUser = async (username) => {
     const postNewUser = await createUserInDB(username);
@@ -6,39 +6,32 @@ export const createNewUser = async (username) => {
         return { success: postNewUser.success, error: postNewUser.error };
     }
     const secretKey = process.env.JWT_SECRET;
-    const userToken = jwt.sign({ user: username }, `${secretKey}/${username}`, { expiresIn: "1h" });
+    const userToken = jwt.sign({ user: username }, secretKey, { expiresIn: "1h" });
     return { success: true, data: userToken };
 };
 const validateTokenPackage = (tokenPackage) => {
     return (typeof tokenPackage === "object" &&
         typeof tokenPackage.user === "string");
 };
-const tokenValidation = (usernameArray, token, secret) => {
+const tokenValidation = (token, secret) => {
     let validUsername = undefined;
     console.log(secret);
-    for (let username of usernameArray) {
-        try {
-            const decoded = jwt.verify(token, `${secret}/${username}`);
-            const validatePackage = validateTokenPackage(decoded);
-            if (validatePackage) {
-                validUsername = decoded.user;
-            }
+    try {
+        const decoded = jwt.verify(token, secret);
+        const validatePackage = validateTokenPackage(decoded);
+        if (validatePackage) {
+            validUsername = decoded.user;
         }
-        catch (error) {
-            console.log(error);
-        }
+    }
+    catch (error) {
+        console.log(error);
     }
     return validUsername;
 };
 export const validateUserToken = async (token) => {
-    const fetchedUsers = await getAllUsernames();
-    console.log(fetchedUsers.data);
-    if (fetchedUsers.error) {
-        return { success: fetchedUsers.success, error: fetchedUsers.error };
-    }
-    const usernameArray = fetchedUsers.data;
+    console.log(token);
     const secretKey = process.env.JWT_SECRET;
-    const validUsername = tokenValidation(usernameArray, token, secretKey);
+    const validUsername = tokenValidation(token, secretKey);
     if (typeof validUsername === "undefined") {
         return { success: false, error: "Invalid Token" };
     }
--- userfuncs/createUser.ts
+++ userfuncs/createUser.ts
@@ -1,4 +1,4 @@
-import { createUserInDB, getAllUsernames } from "../pgFuncs/dbfuncs.js"
+import { createUserInDB, } from "../pgFuncs/dbfuncs.js"
 import jwt from "jsonwebtoken"
 
 type tokenType = {
@@ -12,7 +12,7 @@ export const createNewUser = async(username: string)=>{
         return {success: postNewUser.success, error: postNewUser.error}
     }
     const secretKey = process.env.JWT_SECRET as string
-    const userToken = jwt.sign({user: username}, `${secretKey}/${username}`, {expiresIn: "1h"})
+    const userToken = jwt.sign({user: username}, secretKey, {expiresIn: "1h"})
     return {success: true, data: userToken}
 }
 
@@ -23,12 +23,11 @@ const validateTokenPackage = (tokenPackage: string|jwt.JwtPayload): tokenPackage
     )
 }
 
-const tokenValidation = (usernameArray: string[], token: string, secret: string) =>{
+const tokenValidation = ( token: string, secret: string) =>{
     let validUsername = undefined
     console.log(secret)
-    for (let username of usernameArray){
         try{
-            const decoded = jwt.verify(token, `${secret}/${username}`)
+            const decoded = jwt.verify(token, secret)
             const validatePackage = validateTokenPackage(decoded)
             if (validatePackage){
                 validUsername = decoded.user
@@ -37,19 +36,13 @@ const tokenValidation = (usernameArray: string[], token: string, secret: string)
         catch (error){
             console.log(error)
         }
-    }
     return validUsername
 }
 
 export const validateUserToken = async(token:string)=>{
-    const fetchedUsers = await getAllUsernames()
-    console.log(fetchedUsers.data)
-    if (fetchedUsers.error){
-        return {success: fetchedUsers.success, error: fetchedUsers.error}
-    }
-    const usernameArray = fetchedUsers.data as string[]
+    console.log(token)
     const secretKey = process.env.JWT_SECRET as string
-    const validUsername = tokenValidation(usernameArray, token, secretKey)
+    const validUsername = tokenValidation( token, secretKey)
     if (typeof validUsername === "undefined"){
         return {success: false, error: "Invalid Token"}
     } else return {
